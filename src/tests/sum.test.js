dc6b44a30d709e8e0872a63ac57718452878a2a2
Lav <166986093+lavhuyaar@users.noreply.github.com>
2024-09-12 08:58:21 +0530

    Initial commit

---
+++ src/tests/sum.test.js
@@ -0,0 +1,4 @@
+const sum = require('../sum')
+
+it('test', () => expect(sum(1, 2)).toBe(3))
+it('test 2', () => expect(sum(1, 3)).toBe(4))
---
+++ webpack.common.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: 'main.bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', { targets: 'defaults' }]],
+          },
+        },
+      },
+    ],
+  },
+};
